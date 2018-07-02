0800521d5abdd65b1687d20330d2ba2e3f3c5c22
Vermibus <mgrefka@kainos.com>
2018-07-02 12:30:49 +0200

    Added comments initialization.

--- memory.py
+++ memory.py
@@ -8,6 +8,11 @@ class Memory(object):
         self.memory = bytearray(open(filename, 'rb').read())
 
     def read(self, bytes_count):
+        """
+        Read x bytes from memory
+        :param bytes_count: int
+        :return:
+        """
         self.mp += bytes_count
         return bytes(self.memory[self.mp-bytes_count:self.mp])
 
@@ -15,6 +20,15 @@ class Memory(object):
         return self.mp
 
     def seek(self, offset, offset_type):
+        """
+        Manage memory pointer by offset and offset_type
+        :param offset: int
+            offset for memory pointer, interpretation depends on offset_type
+        :param offset_type: int
+            if 0 set memory pointer to offset
+            if 1 add offset to memory pointer
+        :return: None
+        """
         if offset_type == 0:
             self.mp = offset
         elif offset_type == 1:
